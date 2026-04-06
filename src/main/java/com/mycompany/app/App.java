c003aaf9d20109d17d06cbe549bf1417855e3d7c
SakthiVellavan <phumin029@gmail.com>
2026-04-06 15:57:08 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Make another changes");
+        System.out.println("Modify to trigger poll scm build");
     }
 
     public String getMessage() {
