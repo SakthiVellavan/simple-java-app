7e226a9cbaf80f988093772af0cc51e5382f6e1d
SakthiVellavan <phumin029@gmail.com>
2026-04-06 15:49:46 +0800

    Simulating a 2nd stupid error !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,12 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I love Jenkins";
+    private static final String MESSAGE = "Dummy String";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Dummy String");
+        System.out.println("Make another changes");
     }
 
     public String getMessage() {
