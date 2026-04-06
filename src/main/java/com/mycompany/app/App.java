2a804d70d02629daf2324d985bae6cb2a9c3fdef
SakthiVellavan <phumin029@gmail.com>
2026-04-06 15:46:44 +0800

    Simulating a stupid error !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Make another changes for second time");
+        System.out.println("Dummy String");
     }
 
     public String getMessage() {
