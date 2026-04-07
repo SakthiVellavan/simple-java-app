8ebae4af25f659a4c2f41ef28b9752e0fa248f77
SakthiVellavan <phumin029@gmail.com>
2026-04-07 14:02:59 +0800

    push stupid error

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,12 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I love Jenkins";
+    private static final String MESSAGE = "Dummy String";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project");
+        System.out.println("Modified to trigger stupid error");
     }
 
     public String getMessage() {
